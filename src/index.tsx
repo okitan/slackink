526e3c3e5a29d78ae2f89ffedae5ccbc51f0783d
okitan <okitakunio@gmail.com>
2024-06-03 21:27:23 +0900

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,16 @@ import { Box, Text } from "ink";
 import { marked } from "marked";
 import TerminalRenderer from "marked-terminal";
 
-import type { ActionsBlock, KnownBlock, MrkdwnElement, PlainTextElement, SectionBlock } from "@slack/types";
+import type {
+  ActionsBlock,
+  ContextBlock,
+  ImageBlock,
+  ImageElement,
+  KnownBlock,
+  MrkdwnElement,
+  PlainTextElement,
+  SectionBlock,
+} from "@slack/types";
 
 type Flatten<T> = T extends (infer U)[] ? U : never;
 
@@ -46,7 +55,7 @@ export function convertBlock(key: string, block: KnownBlock): JSX.Element {
         </Text>
       );
     case "image":
-      return <Text key={key}>(image block is not yet supported)</Text>;
+      return convertImage(key, block);
     case "input":
       return <Text key={key}>(input block is not yet supported)</Text>;
     case "section":
@@ -67,9 +76,12 @@ export function convertBlock(key: string, block: KnownBlock): JSX.Element {
 
 export function convertActionElement(
   key: string,
-  element: Flatten<ActionsBlock["elements"]> | undefined,
+  element: Flatten<ActionsBlock["elements"]> | SectionBlock["accessory"],
 ): JSX.Element | undefined {
   if (typeof element === "undefined") return undefined;
+
+  // check image_url just to omit Action in types
+  if (element.type === "image" && "image_url" in element) return convertImage(key, element);
 
   return <Text key={key}>(actions are not yet supported)</Text>;
 }
@@ -86,18 +98,27 @@ export function convertFields(key: string, fields: SectionBlock["fields"]): JSX.
   );
 }
 
-export function convertElement(key: string, element: SectionBlock["text"]): JSX.Element | undefined {
+export function convertElement(
+  key: string,
+  element: Flatten<ContextBlock["elements"]> | SectionBlock["text"],
+): JSX.Element | undefined {
   if (typeof element === "undefined") return;
 
   switch (element.type) {
+    case "image":
+      return convertImage(key, element);
     case "mrkdwn":
       return convertText(key, element);
     case "plain_text":
       return convertText(key, element);
     default:
       const never: never = element;
-      throw new Error(`unknown element type: ${never}`);
+      throw new Error(`unknown element type: ${JSON.stringify(never)}`);
   }
+}
+
+export function convertImage(key: string, element: ImageBlock | ImageElement): JSX.Element {
+  return <Text key={key}>(image is not yet supported)</Text>;
 }
 
 export function convertText(key: string, element: PlainTextElement | MrkdwnElement): JSX.Element {
